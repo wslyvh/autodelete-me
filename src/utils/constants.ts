3609cb5913b53769df8a217adf561c2811f8d886
wslyvh <wslyvh@users.noreply.github.com>
2022-12-12 12:41:33 -0500

    delete nextjs

--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const TITLE = 'Twitter Purge'
-export const DESCRIPTION = 'Automatically purge your Twitter feed'
-export const SITE_URL = 'https://localhost.dev/'
-
-export const DEFAULT_REVALIDATE_PERIOD = 3600
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./src/**/*.{ts,tsx}'],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
